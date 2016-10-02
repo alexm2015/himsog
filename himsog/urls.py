c53e1fd6c3827b1ced29c6f4c82eef362f0c2bd1
Kester <amargo.kristian@gmail.com>
2016-10-02 01:22:20 +0000

    Created view for Category Content List

--- himsog/urls.py
+++ himsog/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
 
 from himsog.views import HomePage
+from himsog.views import CategoryContentListPage
 
 homepatterns = [
     url(r'^$', HomePage.as_view(), name='index'),
@@ -9,4 +10,8 @@ homepatterns = [
 # Add list of patterns
 contentpatterns = []
 
-urlpatterns = homepatterns + contentpatterns
+categorypatterns = [
+	url(r'^cat/(?P<category_slug>[-\w]+)/$', CategoryContentListPage.as_view(), name="content_list_page"),
+]
+
+urlpatterns = homepatterns + categorypatterns + contentpatterns
--- himsog/views/__init__.py
+++ himsog/views/__init__.py
@@ -2,8 +2,25 @@ from django.http import HttpResponse
 from django.views.generic import View
 from django.shortcuts import render
 
-
+from himsog.models import Category
+from himsog.models import Content
 class HomePage(View):
     
     def get (self, request):
-        return render(request, 'home.html')
+
+        categories = Category.objects.all()
+
+        return render(request, 'home.html', {'categories':categories})
+
+class CategoryContentListPage(View):
+
+	def get(self, request, category_slug=None):
+
+		context = dict()
+		cat_slug = category_slug
+
+		cat = Category.objects.get(slug_name=cat_slug)
+		contents = Content.objects.filter(category=cat)
+
+		context['contents'] = contents
+		return render(request, 'category_content_list.html', context) 
