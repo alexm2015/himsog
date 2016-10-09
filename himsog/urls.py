152f4885f5419454541de76706a07097fc670bbb
jpailma <jonas.pailma@yahoo.com.ph>
2016-10-09 19:32:38 +0800

    Added backend implementation for View Content Page

--- himsog/urls.py
+++ himsog/urls.py
@@ -1,13 +1,17 @@
 from django.conf.urls import url
 
+from himsog.models import Content
 from himsog.views import HomePage
-from himsog.views import CategoryContentListPage
+from himsog.views import ContentPage
+
 
 homepatterns = [
     url(r'^$', HomePage.as_view(), name='index'),
 ]
 
 # Add list of patterns
-contentpatterns = []
+contentpatterns = [
+	url(r'^content/(?P<id>[0-9]+)/$', ContentPage.as_view(), name="content_page"),
+]
 
 urlpatterns = homepatterns + contentpatterns
