40f8f99e94117fe4e31b829fafc9e7d51e2f34e6
Otap <janotanerang@gmail.com>
2016-11-02 11:45:43 +0800

    Removed Comment model from populate script

--- himsog/management/commands/populate_db.py
+++ himsog/management/commands/populate_db.py
@@ -1,4 +1,3 @@
-import os
 import random
 
 from django.core.files.base import File
@@ -6,8 +5,9 @@ from django.core.files.temp import NamedTemporaryFile
 from django.core.management.base import BaseCommand
 from sampledatahelper.helper import SampleDataHelper
 
-from himsog.models import Category, Content, Comment, ContentImage
-from settings.settings import BASE_DIR
+from himsog.models import Category
+from himsog.models import Content
+from himsog.models import ContentImage
 
 
 class Command(BaseCommand):
@@ -19,7 +19,7 @@ class Command(BaseCommand):
     sdh = SampleDataHelper(seed=1234567890)
 
 
-    def generate_content(self, category, instances, comments=0, images=0):
+    def generate_content(self, category, instances, images=0):
 
         for _ in range(instances):
             content = Content.objects.create(category=category,
@@ -40,10 +40,6 @@ class Command(BaseCommand):
 
                 content.images.add(content_image)
 
-            for _ in range(random.randint(0, comments)):
-                comment = Comment.objects.create(comment=self.sdh.words(1, 250))
-                content.comments.add(comment)
-
             content.save()
 
     def handle(self, *args, **options):
@@ -51,15 +47,15 @@ class Command(BaseCommand):
         print('Populating database')
 
         category_food, _ = Category.objects.get_or_create(name='Food And Supplements')
-        self.generate_content(category_food, instances=20, comments=10, images=4)
+        self.generate_content(category_food, instances=5, images=4)
 
         category_service, _ = Category.objects.get_or_create(name='Services')
-        self.generate_content(category_service, instances=25, comments=10, images=3)
+        self.generate_content(category_service, instances=2, images=3)
 
         category_event, _ = Category.objects.get_or_create(name='Events')
-        self.generate_content(category_event, instances=10, comments=10, images=10)
+        self.generate_content(category_event, instances=2, images=10)
 
         category_article, _ = Category.objects.get_or_create(name='Articles')
-        self.generate_content(category_article, instances=30, comments=10, images=2)
+        self.generate_content(category_article, instances=1, images=2)
 
         print('Database population complete')
